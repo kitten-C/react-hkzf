fb7c003ec60d6e32dff8350929d7658572ad5857
kittenbox <48595625+kitten-C@users.noreply.github.com>
2019-07-23 15:28:46 +0800

    渲染出地图,还没有进行定位

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 
 import Home from './components/Home'
+import MapCont from './pages/Map'
 
 export default class extends React.Component {
   render() {
@@ -11,6 +12,7 @@ export default class extends React.Component {
         {/* /重定向到/home */}
         <Route path="/" exact render={() => <Redirect to="/home" />} />
         <Route path="/home" component={Home} />
+        <Route path="/map" component={MapCont} />
       </Router>
     )
   }
