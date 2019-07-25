b36de2fea66b281d8e3e89da6f08595896a78d9e
kittenbox <48595625+kitten-C@users.noreply.github.com>
2019-07-25 19:18:25 +0800

    citylist

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -3,13 +3,34 @@ import React from 'react'
 import axios from 'axios'
 
 import {NavBar} from 'antd-mobile'
-import {List} from 'react-virtualized'
+import {List, AutoSizer} from 'react-virtualized'
 
 import {getCurrentCity} from '../../utils'
 
 import './index.scss'
 
 // 处理城市列表数据
+// 1. 引入示例
+// 2. 修改示例数据
+// 3. 导入高阶组件(AutoSizer)处理自动宽高
+// 4. 父盒子设置高度100%
+// 5. 解决高度超过视口问题
+//  5.1 父盒子设施padding-top45px
+//  5.2 bar设置margin-top -45px
+//  5.3 解释 自动获取宽高不会获取padding值
+// 6. 替换真实数据，把函数放入组件中，并修改格式
+// 7. 计算高度
+// 8. 添加右侧索引
+// 9. 滚动高亮
+// 10. 点击轮到相应的位置
+
+// 设置常量
+// 索引高度
+const TITLE_HEIGHT = 36
+// 城市列表高度
+const CITY_HEIGHT = 50
+
+// 处理所有城市数据
 const formatCityList = res => {
   const cityList = {}
   res.forEach(v => {
@@ -24,28 +45,29 @@ const formatCityList = res => {
   return {cityList, cityInitial}
 }
 
-// react-virtualized插件示例代码
-const list = Array.from(new Array(10000)).map((v, i) => {
-  return `假数据 20点58分${i}`
-})
-console.log(list)
+// 处理列表城市头部索引
+const formatCityIndex = initial => {
+  switch (initial) {
+    case '#':
+      return '本地城市'
+    case 'hot':
+      return '热门城市'
 
-function rowRenderer({
-  key, // Unique key within array of rows
-  index, // Index of row within collection
-  isScrolling, // The List is currently being scrolled
-  isVisible, // This row is visible within the List (eg it is not an overscanned row)
-  style // Style object to be applied to row (to position it)
-}) {
-  return (
-    <div key={key} style={style}>
-      {list[index]}
-    </div>
-  )
+    default:
+      return initial.toUpperCase()
+  }
 }
+// react-virtualized插件示例代码
+// const list = Array.from(new Array(10000)).map((v, i) => {
+//   return `假数据 20点58分${i}`
+// })
 
 export default class extends React.Component {
-  state = {}
+  state = {
+    cityList: {},
+    cityInitial: [],
+    activeInitial: 0
+  }
   // 1 获取处理好的城市数据
   async fetchCityList() {
     // 1.1 获取所有城市列表数据
@@ -69,20 +91,85 @@ export default class extends React.Component {
     const {label, value} = await getCurrentCity()
 
     // 1.3.1 热门数据城市添加进数据中
-    cityList['curCity'] = [{label, value}]
+    cityList['#'] = [{label, value}]
     cityInitial.unshift('#')
 
-    console.log(cityList, cityInitial)
+    // console.log()
+    this.setState({
+      cityList,
+      cityInitial
+    })
   }
 
-  componentDidMount() {
+  listRef = React.createRef()
+
+  // 动态获取高度
+  calcRowHeight = ({index}) => {
+    const {cityInitial, cityList} = this.state
+
+    const initial = cityInitial[index]
+    const list = cityList[initial]
+
+    return TITLE_HEIGHT + CITY_HEIGHT * list.length
+  }
+
+  // 插件中的方法
+  rowRenderer = ({key, index, style}) => {
+    const {cityInitial, cityList} = this.state
+
+    const initial = cityInitial[index]
+    const list = cityList[initial]
+    // console.log(list)
+    return (
+      <div key={key} style={style} className="city">
+        <div className="title">{formatCityIndex(initial)}</div>
+        {list.map(v => (
+          <div className="name" key={v.label}>
+            {v.label}
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  //渲染索引值
+  renderCityIntial = () => {
+    const {cityInitial, activeInitial} = this.state
+    return cityInitial.map((v, i) => (
+      <div
+        className="city-index-item"
+        key={i}
+        onClick={() => this.goCityList(i)}
+      >
+        <span className={activeInitial === i ? 'index-active' : ''}>
+          {v === 'hot' ? '热' : v.toUpperCase()}
+        </span>
+      </div>
+    ))
+  }
+  // 页面滚动时触发的函数
+  onRowsRendered = ({startIndex}) => {
+    if (this.state.activeInitial === startIndex) return
+    this.setState({
+      activeInitial: startIndex
+    })
+  }
+
+  // 点击右侧索引
+  goCityList = i => {
+    console.log(i)
+    this.listRef.current.scrollToRow(i)
+    // console.log(this.listRef.current)
+  }
+
+  async componentDidMount() {
     // 获取所有城市数据
-    this.fetchCityList()
+    await this.fetchCityList()
   }
 
   render() {
     return (
-      <div>
+      <div className="city_list">
         {/* 导航栏 */}
         <NavBar
           className="nvaBar"
@@ -93,13 +180,22 @@ export default class extends React.Component {
           城市选择
         </NavBar>
         {/* 城市数据渲染 */}
-        <List
-          width={300}
-          height={300}
-          rowCount={list.length}
-          rowHeight={20}
-          rowRenderer={rowRenderer}
-        />
+        <AutoSizer>
+          {({height, width}) => (
+            <List
+              width={width}
+              height={height}
+              rowCount={this.state.cityInitial.length}
+              rowHeight={this.calcRowHeight}
+              rowRenderer={this.rowRenderer}
+              onRowsRendered={this.onRowsRendered}
+              ref={this.listRef}
+              scrollToAlignment="start"
+            />
+          )}
+        </AutoSizer>
+        {/* 右侧索引 */}
+        <div className="city-index">{this.renderCityIntial()}</div>
       </div>
     )
   }
